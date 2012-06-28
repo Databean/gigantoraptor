bf557762b18519810093214b663a317e360ff6ab
Andrew Chen <andrewtheannihilator@gmail.com>
2012-06-28 00:46:05 -0700

    initial humanVisPlayer added, made HumanPlayer internals protected

--- include/HumanPlayer.h
+++ include/HumanPlayer.h
@@ -17,7 +17,7 @@ public:
 	virtual void handleEvent(const GameState&,const SDL_Event&);
 	virtual void draw(const ArimaaGame&, const GameState&);
 	
-private:
+protected:
 	bool color;
 	pos select_i_1, select_j_1, select_i_2, select_j_2, select_i_3, select_j_3;
 	pos piece;
---
+++ src/HumanVisPlayer.cpp
@@ -0,0 +1,11 @@
+#include "HumanVisPlayer.h"
+#include <SDL_opengl.h>
+#include <gl.h>
+#include "ArimaaGame.h"
+#include <iostream>
+
+void HumanVisPlayer::draw(const ArimaaGame& a, const GameState& g){
+
+
+
+}
