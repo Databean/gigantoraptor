7d1ed6c34cd95a918a1a8e46585866fceea44596
A. Cody Schuffelen <astronomix@gmail.com>
2014-05-05 16:40:24 -0500

    Removed SDL OpenGL rendering configuration to fix drawing

--- src/CApp.cpp
+++ src/CApp.cpp
@@ -29,25 +29,8 @@ bool CApp::init() {
 		cout << "initialized SDL_ttf" << endl;
 	}
 	
-	
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE,            8);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,          8);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,           8);
-	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,          8);
-	
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,          16);
-	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE,         32);
-	
-	SDL_GL_SetAttribute(SDL_GL_ACCUM_RED_SIZE,      8);
-	SDL_GL_SetAttribute(SDL_GL_ACCUM_GREEN_SIZE,    8);
-	SDL_GL_SetAttribute(SDL_GL_ACCUM_BLUE_SIZE,     8);
-	SDL_GL_SetAttribute(SDL_GL_ACCUM_ALPHA_SIZE,    8);
-	
-	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS,  1);
-	
-	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES,  2);
-	
 	if((Surf_Display = SDL_SetVideoMode(windowWidth, windowHeight, 32, SDL_HWSURFACE | SDL_OPENGL)) == NULL) {
+		cout << "Failed to create OpenGL surface: " << SDL_GetError() << std::endl;
 		return false;
 	}
 	
